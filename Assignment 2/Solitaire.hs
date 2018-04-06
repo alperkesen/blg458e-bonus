bdc679b912f38517fd6d4d086a7a64c5979c3217
alperkesen <kesen.alper@gmail.com>
2018-04-06 20:15:46 +0300

    Write runGame function

--- Assignment 2/Solitaire.hs
+++ Assignment 2/Solitaire.hs
@@ -13,7 +13,7 @@ data Color = Red | Black deriving (Eq, Show)
 data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Show)
 data Rank = Num Int | Jack | Queen | King | Ace deriving (Eq, Show)
 data Card = Card { suit :: Suit, rank :: Rank } deriving (Eq, Show)
-data Move = Draw | Discard Card deriving (Eq, Show)
+data Move = Draw | Discard { card :: Card } deriving (Eq, Show)
 data State = Start | Continue | End deriving (Eq, Show)
 
 
@@ -58,3 +58,19 @@ score cs g
   | otherwise = if allSameColor cs then div (g - s) 2 else g - s
     where
       s = sumCards cs
+
+runGame :: [Card] -> [Move] -> Int -> Int
+runGame cs ms g = playGame Start cs ms []
+  where
+    playGame :: State -> [Card] -> [Move] -> [Card] -> Int
+    playGame Start cs ms _  = playGame Continue cs ms []
+    playGame End _ _ hs = score hs g
+    playGame _ cs [] hs = playGame End cs [] hs
+    playGame Continue cards@(c:cs) (m:ms) hs
+      | m /= Draw                     = playGame Continue cards ms (removeCard hs dc)
+      | m == Draw && cards == []      = playGame End cs ms hs
+      | m == Draw && sumCards hs' > g = playGame End cs ms hs'
+      | otherwise                     = playGame Continue cs ms hs'
+      where
+        dc = card m
+        hs' = c : hs
